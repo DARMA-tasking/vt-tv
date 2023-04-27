e15e33e9ca0c5d13b537ca47717d868ffc458214
Jonathan Lifflander <jliffla@sandia.gov>
2023-04-27 16:32:01 -0700

    #5: lib: add brotli to the TPLs for json compression

--- cmake/load_packages.cmake
+++ cmake/load_packages.cmake
@@ -9,5 +9,14 @@ add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/json)
 set(FMT_LIBRARY fmt)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/fmt)
 
+# brotli library always included in the build
+set(BROTLI_DISABLE_TESTS ON)
+# we need to disable bundled mode so it will install properly
+set(BROTLI_BUNDLED_MODE OFF)
+set(BROTLI_BUILD_PORTABLE ON)
+set(BROTLI_LIBRARY brotlicommon-static brotlienc-static brotlidec-static)
+add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/brotli)
+
+
 include(cmake/load_vtk_package.cmake)
 include(cmake/load_nanobind_package.cmake)
