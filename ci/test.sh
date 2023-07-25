48d66eeada1c5a587fcc114d1c02124c7ff33ac1
Pierre Pébay <pebay.pierre@gmail.com>
2023-07-25 15:50:01 -0400

    #26: Fix output directory in CI

--- ci/test.sh
+++ ci/test.sh
@@ -3,11 +3,11 @@
 set -e
 
 pushd /opt/build/vt-tv
+mkdir -p /opt/src/vt-tv/output/
 success_flag=0
 ctest --output-on-failure || success_flag=1
 # We collect the test logs for exporting
 echo "this is the Success flag: ${success_flag}"
-mkdir -p /opt/src/vt-tv/output/
 mkdir -p /tmp/artifacts/
 cp /opt/build/vt-tv/Testing/Temporary/LastTest.log /tmp/artifacts/
 echo ${success_flag} > /tmp/artifacts/success_flag.txt
