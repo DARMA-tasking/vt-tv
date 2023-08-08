9226de8672214fb53bb5487d42efaca0fa2bbf4b
Jonathan Lifflander <jliffla@sandia.gov>
2023-08-08 13:59:24 -0700

    #46: cmake: add option to disable Python when building with VT

--- cmake/load_packages.cmake
+++ cmake/load_packages.cmake
@@ -1,24 +1,33 @@
 
 # json library always included in the build
-set(JSON_BuildTests OFF)
-set(JSON_MultipleHeaders ON)
-set(JSON_LIBRARY nlohmann_json)
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/json)
+if (NOT TARGET nlohmann_json)
+  set(JSON_BuildTests OFF)
+  set(JSON_MultipleHeaders ON)
+  set(JSON_LIBRARY nlohmann_json)
+  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/json)
+endif()
 
 # fmt always included in the build
-set(FMT_LIBRARY fmt)
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/fmt)
+if (NOT TARGET fmt)
+  set(FMT_LIBRARY fmt)
+  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/fmt)
+endif()
 
-# brotli library always included in the build
-set(BROTLI_DISABLE_TESTS ON)
-# we need to disable bundled mode so it will install properly
-set(BROTLI_BUNDLED_MODE OFF)
-set(BROTLI_BUILD_PORTABLE ON)
-set(BROTLI_LIBRARY brotlicommon-static brotlienc-static brotlidec-static)
-add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/brotli)
+if (NOT TARGET brotli)
+  # brotli library always included in the build
+  set(BROTLI_DISABLE_TESTS ON)
+  # we need to disable bundled mode so it will install properly
+  set(BROTLI_BUNDLED_MODE OFF)
+  set(BROTLI_BUILD_PORTABLE ON)
+  set(BROTLI_LIBRARY brotlicommon-static brotlienc-static brotlidec-static)
+  add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/brotli)
+endif()
 
 set(YAML_LIBRARY yaml-cpp)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/lib/yaml-cpp)
 
 include(cmake/load_vtk_package.cmake)
-include(cmake/load_nanobind_package.cmake)
+
+if (vt_tv_python_bindings_enabled)
+  include(cmake/load_nanobind_package.cmake)
+endif()
