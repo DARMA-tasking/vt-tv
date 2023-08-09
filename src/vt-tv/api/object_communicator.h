4df866baddc5371480131c26adb2742297e0e6b6
Jonathan Lifflander <jliffla@sandia.gov>
2023-08-09 16:11:43 -0700

    #48: communicator: reformat and cleanup the ObjectCommunicator

--- src/vt-tv/api/object_communicator.h
+++ src/vt-tv/api/object_communicator.h
@@ -17,17 +17,41 @@ namespace vt::tv {
 struct Object;
 
 /**
- *  A class holding received and sent messages for an object.
+ * \struct ObjectCommunicator
+ *
+ * \brief A class holding received and sent messages for an object.
  */
-struct ObjectCommunicator
-{
-private:
-  ElementIDType object_id_;
-  std::map<ElementIDType, double> received_;
-  std::map<ElementIDType, double> sent_;
+struct ObjectCommunicator {
 
   /**
-   * Summarize one-way communicator properties and check for errors.
+   * \brief Construct an \c ObjectCommunicator without any edges
+   *
+   * \param[in] id_in the object id
+   */
+  explicit ObjectCommunicator(ElementIDType id_in)
+    : object_id_(id_in)
+  {}
+
+  /**
+   * \brief Construct an \c ObjectCommunicator with edges
+   *
+   * \param[in] id_in the object id
+   * \param[in] recv_in receive edges
+   * \param[in] sent_in send edges
+   */
+  ObjectCommunicator(
+    ElementIDType id_in,
+    std::map<ElementIDType, double> recv_in,
+    std::map<ElementIDType, double> sent_in
+  ) : object_id_(id_in),
+      received_(recv_in),
+      sent_(sent_in)
+  { }
+
+  /**
+   * \brief Summarize one-way communicator properties and check for errors.
+   *
+   * \param[in] direction the direction to summarize edges
    */
   std::vector<double> summarizeUnidirectional(std::string direction) const {
     // Initialize list of volumes
@@ -53,40 +77,42 @@ private:
 
     return volumes;
   }
-public:
+
   /**
-   * \brief get id of object for this communicator
+   * \brief Get the id of object for this communicator
    *
    * \return id
    */
   ElementIDType getObjectId() const { return object_id_; };
 
   /**
-   * Return all from_object=volume pairs received by object.
+   * \brief Return all from_object=volume pairs received by object.
    */
-  std::map<ElementIDType, double> getReceived() const { return this->received_; };
+  std::map<ElementIDType, double> getReceived() const {
+    return this->received_;
+  };
 
   /**
-   * Return the volume of a message received from an object if any.
+   * \brief Return the volume of a message received from an object if any.
    */
   double getReceivedFromObject(ElementIDType id) const {
     return this->received_.at(id);
   }
 
   /**
-   * Return all to_object=volume pairs sent from object.
+   * \brief Return all to_object=volume pairs sent from object.
    */
   std::map<ElementIDType, double> getSent() const { return this->sent_; };
 
   /**
-   * Return the volume of a message sent to an object if any.
+   * \brief Return the volume of a message sent to an object if any.
    */
   double getSentToObject(ElementIDType id) const {
     return this->sent_.at(id);
   }
 
   /**
-   * Summarize communicator properties and check for errors.
+   * \brief Summarize communicator properties and check for errors.
    */
   std::pair<std::vector<double>, std::vector<double>> summarize() const {
     // Summarize sent communications
@@ -98,29 +124,40 @@ public:
     return std::make_pair(w_sent, w_recv);
   }
 
+  /**
+   * \brief Add received edge
+   *
+   * \param[in] from_id the from object id
+   * \param[in] bytes the number of bytes
+   */
   void addReceived(ElementIDType from_id, double bytes) {
     this->received_.insert(std::make_pair(from_id, bytes));
-    if (from_id == this->object_id_) fmt::print("Object {} receiving communication from myself\n",this->object_id_);
+    if (from_id == this->object_id_) {
+      fmt::print(
+        "Object {} receiving communication from myself\n", this->object_id_
+      );
+    }
   }
 
+  /**
+   * \brief Add sent edge
+   *
+   * \param[in] to_id the to object id
+   * \param[in] bytes the number of bytes
+   */
   void addSent(ElementIDType to_id, double bytes) {
     this->sent_.insert(std::make_pair(to_id, bytes));
-    if (to_id == this->object_id_) fmt::print("Object {} sending communication to myself\n",this->object_id_);
+    if (to_id == this->object_id_) {
+      fmt::print(
+        "Object {} sending communication to myself\n", this->object_id_
+      );
+    }
   }
 
-  ObjectCommunicator(ElementIDType id_in)
-  : object_id_(id_in)
-  , received_()
-  , sent_()
-  {}
-  ObjectCommunicator(ElementIDType id_in,
-                      std::map<ElementIDType, double> recv_in,
-                      std::map<ElementIDType, double> sent_in)
-  : object_id_(id_in)
-  , received_(recv_in)
-  , sent_(sent_in)
-  {}
-  ~ObjectCommunicator() = default;
+private:
+  ElementIDType object_id_;                  /**< The object id */
+  std::map<ElementIDType, double> received_; /**< The received edges */
+  std::map<ElementIDType, double> sent_;     /**< The sent edges */
 };
 
 } /* end namesapce vt::tv */
