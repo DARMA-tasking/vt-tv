96a14b2a368f7e251eebb425f782193ab41d3b32
pierrepebay <pebay.pierre@gmail.com>
2023-09-08 15:38:35 +0200

    #51: Fix supported Python versions

--- cmake/load_nanobind_package.cmake
+++ cmake/load_nanobind_package.cmake
@@ -1,4 +1,10 @@
-find_package(Python 3.8...3.9 COMPONENTS Interpreter Development.Module REQUIRED)
+find_package(Python COMPONENTS Interpreter Development.Module REQUIRED)
+
+if(NOT (${Python_VERSION_MAJOR} EQUAL 3 AND 
+       (${Python_VERSION_MINOR} EQUAL 8 OR ${Python_VERSION_MINOR} EQUAL 9)))
+    message(FATAL_ERROR "Python version must be 3.8 or 3.9")
+endif()
+
 
 # Detect the installed nanobind package and import it into CMake
 message(STATUS "Python executable: ${Python_EXECUTABLE}")
