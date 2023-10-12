d978110e7beffb2913f7fa1ef0c23cac2d706a04
pierrepebay <pebay.pierre@gmail.com>
2023-10-13 01:49:51 +0200

    #16: bindings: Continue binding setup

---
+++ bindings/python/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name='tv',
+  version='0.1',
+  description='Virtual Transport Task Visualizer',
+  author='Jonathan Lifflander, Pierre Pebay',
+  package_data={'': ['/home/pierrelp/Develop/NGA/vt-tv/build/bindings/python/tv.cpython-38-x86_64-linux-gnu.so']}
+)
