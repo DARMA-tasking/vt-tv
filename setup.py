008e79b9f658d7708dccdff1ac282d112c4dc8c1
pierrepebay <pebay.pierre@gmail.com>
2023-11-07 18:45:23 +0100

    #16: bindings: Specify cmake build in setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,42 @@
-from setuptools import setup, Extension
-from setuptools.command.build_ext import build_ext
+import os
+import re
+import subprocess
 import sys
-import setuptools
-import nanobind
+from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
 
-class get_nanobind_include(object):
-  """Helper class to determine the nanobind include path
-  The purpose of this class is to postpone importing nanobind
-  until it is actually installed, so that the `get_include()`
-  method can be invoked."""
 
-  def __str__(self):
-    import nanobind
-    return nanobind.include_dir()
+class CMakeExtension(Extension):
+  def __init__(self, name, sourcedir=''):
+    Extension.__init__(self, name, sources=[])
+    self.sourcedir = os.path.abspath(sourcedir)
 
-ext_modules = [
-  Extension(
-    'vttv',
-    ['bindings/python/tv.cc'],
-    include_dirs=[
-      # Path to nanobind headers
-      get_nanobind_include(),
-      './'  # Assuming the root of your C++ project is the current directory
-    ],
-    language='c++'
-  ),
-]
+
+class CMakeBuild(build_ext):
+  def run(self):
+    try:
+      out = subprocess.check_output(['cmake', '--version'])
+    except OSError:
+      raise RuntimeError("CMake must be installed to build the following extensions: " +
+                 ", ".join(e.name for e in self.extensions))
+
+    for ext in self.extensions:
+      self.build_extension(ext)
+
+  def build_extension(self, ext):
+    extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+    cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+            '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+    cfg = 'Debug' if self.debug else 'Release'
+    build_args = ['--config', cfg]
+
+    if not os.path.exists(self.build_temp):
+      os.makedirs(self.build_temp)
+
+    subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
+    subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
 
 setup(
   name='vttv',
@@ -36,7 +47,9 @@ setup(
   description='Virtual Transport Task Visualizer',
   long_description='',
   ext_modules=ext_modules,
-  setup_requires=['nanobind>=1.3.2'],
-  cmdclass={'build_ext': build_ext},
+  setup_requires=['nanobind>=1.3.2', 'cmake>=3.17.0'],
+  packages=find_packages(),
+  ext_modules=[CMakeExtension('vttv', 'bindings/python')],
+  cmdclass=dict(build_ext=CMakeBuild),
   zip_safe=False,
 )
