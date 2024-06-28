586b3d44399a93910a6ea0b7a817244f7844424a
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-06-28 14:36:50 +0200

    #39: fix build script invalid variable name and add option to fix python bad detection

--- dev-build.sh
+++ dev-build.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Paths
-VT-TV_DIR="/path/to/vt-tv/source"
+VT_TV_DIR="/path/to/vt-tv/source"
 BUILD_DIR="/path/to/vt-tv/build"
 VTK_DIR="/path/to/compiled/VTK"
 JOBS="2"  # for instance, modify as needed
@@ -15,7 +15,9 @@ cd "${BUILD_DIR}"
 # rm -rf CMakeCache.txt
 
 cmake -B "${BUILD_DIR}" \
-  -DVTK_DIR="${VTK_DIR}" \
-  "${VT-TV_DIR}"
+  -DVTK_DIR=${VTK_DIR} \
+  -DPython_EXECUTABLE="$(which python)" \
+  -DPython_INCLUDE_DIRS=$(python -c "import sysconfig; print(sysconfig.get_path('include'))") \
+  "${VT_TV_DIR}"
 
 time cmake --build . --parallel -j"${JOBS}"
