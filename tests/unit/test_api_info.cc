b6f08f9819201414c76ba59994e063cf92b16905
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-01 16:43:00 +0200

    #32: introduce info unit test

---
+++ tests/unit/test_api_info.cc
@@ -0,0 +1,77 @@
+/*
+//@HEADER
+// *****************************************************************************
+//
+//                           test_json_reader.cc
+//             DARMA/vt-tv => Virtual Transport -- Task Visualizer
+//
+// Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+// (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+// Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from this
+//   software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact darma@sandia.gov
+//
+// *****************************************************************************
+//@HEADER
+*/
+
+#include <gtest/gtest.h>
+#include "test_harness.h"
+
+#include "vt-tv/api/info.h"
+
+#include <fmt-vt/format.h>
+
+#include "cmake_config.h"
+
+#include <string>
+#include <filesystem>
+#include <iostream>
+#include <variant>
+
+namespace vt::tv::tests::unit {
+
+struct TestInfo : TestHarness { };
+
+TEST_F(TestInfo, test_info_1) {
+  
+  /* TODO */
+
+  std::unique_ptr<Info> info = std::make_unique<Info>();
+
+  /* 
+  Common Assertions: EXPECT_EQ(a, b), EXPECT_TRUE(condition)
+  Common print: fmt::print(
+      format, var1, var2 etc.
+    );
+  */
+  EXPECT_EQ(info->getNumRanks(), 0);
+}
+
+} // end namespace vt::tv::tests::unit
