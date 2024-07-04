0ea6db2be23b703fd51316a3b224d05d116a64ab
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 13:19:41 +0200

    #32: fix syntax error

--- ci/test-bindings.sh
+++ ci/test-bindings.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-# bash -c ". /opt/conda/etc/profile.d/conda.sh && conda activate deves"
-# . /opt/conda/etc/profile.d/conda.sh && conda activate deves
+. /opt/conda/etc/profile.d/conda.sh && conda activate deves
 pip install PyYAML
-python /opt/src/vt-tv/tests/test_bindings.py"
+python /opt/src/vt-tv/tests/test_bindings.py
