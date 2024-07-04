430a2b474de016344f1ee28e9dfc84ac9e2cfbe7
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 13:46:10 +0200

    #32: fix dependencies

--- ci/test-bindings.sh
+++ ci/test-bindings.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Build
 . /opt/conda/etc/profile.d/conda.sh && conda activate deves
 pip install PyYAML
+pip install /opt/src/vt-tv
+
+# Test
 python /opt/src/vt-tv/tests/test_bindings.py
