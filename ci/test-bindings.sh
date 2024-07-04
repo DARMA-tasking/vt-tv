1f6d08f4b41bb5e41d5eb31a159ed89d6980e5df
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 16:35:14 +0200

    #32: add comments to test_binsings script

--- ci/test-bindings.sh
+++ ci/test-bindings.sh
@@ -2,10 +2,13 @@
 
 set -e
 
+# Start X virtual frame buffer
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
-# Build
+# Activate conda environment
 . /opt/conda/etc/profile.d/conda.sh && conda activate deves
+
+# Build
 pip install PyYAML
 pip install /opt/src/vt-tv
 
