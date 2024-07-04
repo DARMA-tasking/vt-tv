eaa08afb803053bf85a8a40dfc6190acc4292e5f
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 13:46:10 +0200

    #32: fix dependencies

--- ci/test-bindings-entrypoint.sh
+++ ci/test-bindings-entrypoint.sh
@@ -1,7 +1,4 @@
-# X11 display
+#!/usr/bin/env bash
+
+# Set X11 display
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-
-# Prepare conda environment
-. /opt/conda/etc/profile.d/conda.sh && conda activate deves
-pip install /opt/src/vt-tv
-
