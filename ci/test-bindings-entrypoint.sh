54a3f4d3f60462707517876a0853bc6090dcb994
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 13:35:08 +0200

    #32: fix python dependency

--- ci/test-bindings-entrypoint.sh
+++ ci/test-bindings-entrypoint.sh
@@ -1,2 +1,8 @@
+# X11 display
 Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
-. /opt/conda/etc/profile.d/conda.sh && conda activate deves && pip install /opt/src/vt-tv && pip install PyYAML
+
+# Prepare conda environment
+. /opt/conda/etc/profile.d/conda.sh && conda activate deves
+pip install PyYAML
+pip install /opt/src/vt-tv
+
--- ci/test-bindings.sh
+++ ci/test-bindings.sh
@@ -2,5 +2,4 @@
 
 set -e
 
-. /opt/conda/etc/profile.d/conda.sh && conda activate deves
 python /opt/src/vt-tv/tests/test_bindings.py
