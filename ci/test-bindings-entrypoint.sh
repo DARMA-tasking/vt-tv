81966ded815b651020b4c4878410284c14a83865
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-04 13:42:16 +0200

    #32: fix ci error

--- ci/test-bindings-entrypoint.sh
+++ ci/test-bindings-entrypoint.sh
@@ -3,6 +3,5 @@ Xvfb :99 -screen 0 1024x768x24 > /dev/null 2>&1 &
 
 # Prepare conda environment
 . /opt/conda/etc/profile.d/conda.sh && conda activate deves
-pip install PyYAML
 pip install /opt/src/vt-tv
 
