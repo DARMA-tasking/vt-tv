d02d2944f8d324e5e0ec84a58660231927804031
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-17 17:58:19 +0200

    #32: add simple coverage report in artifacts

--- ci/test.sh
+++ ci/test.sh
@@ -15,4 +15,15 @@ echo ${success_flag} > /tmp/artifacts/success_flag.txt
 ls /tmp/artifacts
 # Simply output the LastTest.log to screen
 cat /opt/build/vt-tv/Testing/Temporary/LastTest.log
+
+# coverage reporting
+pushd /opt/src/vt-tv/output
+lcov --capture --directory /opt/build/vt-tv --output-file lcov_vt-tv_test.info
+lcov --remove lcov_vt-tv_test.info -o lcov_vt-tv_test_no_deps.info '*/lib/*' '/usr/include/*' '*/vtk/*' '*/tests/*'
+lcov --list lcov_vt-tv_test_no_deps.info
+# add simple coverage artifact
+lcov --list lcov_vt-tv_test_no_deps.info > lcov-list-report.txt
+cp /opt/src/vt-tv/output/lcov-list-report.txt /tmp/artifacts/
+popd
+
 popd
