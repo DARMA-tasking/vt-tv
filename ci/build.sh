3c8218a53513b0641cdfd47e4af18fb97988cf67
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-23 13:33:45 +0200

    #32: fix path in shell script

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,11 @@
+set -ex
+
 # Calls build script with CI configuration
 # (Specific path, enable tests and coverage)
 bash -c "CMAKE_BINARY_DIR=/opt/build/vt-tv \
     VTK_DIR=/opt/build/vtk \
     VT_TV_TESTS_ENABLED=ON \
     VT_TV_COVERAGE_ENABLED=ON \
-    /home/thomas/repositories/vt-tv/build.sh"
+    /opt/src/vt-tv/build.sh"
 
 echo "VT-TV build success"
