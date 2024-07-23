d6d8d91774f62ca8ddca47c684743846a1f410f6
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-23 13:18:11 +0200

    #32: update build script for CI

--- ci/build.sh
+++ ci/build.sh
@@ -4,8 +4,7 @@ CMAKE_BINARY_DIR=/opt/build/vt-tv
     VTK_DIR=/opt/build/vtk
     VT_TV_TESTS_ENABLED=ON
     VT_TV_COVERAGE_ENABLED=ON
-    /home/thomas/repositories/vt-tv/build.sh
-    #/opt/src/vt-tv/build.sh
-    
+    /opt/src/vt-tv/build.sh
+    #/home/thomas/repositories/vt-tv/build.sh # local test
 
 echo "VT-TV build success"
