d3e0df2ea186a060cb505771f0c47d339bf5492f
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-23 22:23:32 +0200

    #32: turn VT_TV_WERROR_ENABLED to ON in CI

--- ci/build.sh
+++ ci/build.sh
@@ -10,6 +10,7 @@ bash -c "VT_TV_BUILD_DIR=/opt/build/vt-tv \
     VTK_DIR=/opt/build/vtk \
     VT_TV_TESTS_ENABLED=ON \
     VT_TV_COVERAGE_ENABLED=${VT_TV_COVERAGE_ENABLED:-OFF} \
+    VT_TV_WERROR_ENABLED=ON \
     /opt/src/vt-tv/build.sh"
 
 echo "VT-TV build success"
