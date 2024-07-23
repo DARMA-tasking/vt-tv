fdfd3789f43509e1a7074122ee167ac32ca51016
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-23 13:30:38 +0200

    #32: fix bad shell command

--- ci/build.sh
+++ ci/build.sh
@@ -1,10 +1,9 @@
 # Calls build script with CI configuration
 # (Specific path, enable tests and coverage)
-CMAKE_BINARY_DIR=/opt/build/vt-tv
-    VTK_DIR=/opt/build/vtk
-    VT_TV_TESTS_ENABLED=ON
-    VT_TV_COVERAGE_ENABLED=ON
-    /opt/src/vt-tv/build.sh
-    #/home/thomas/repositories/vt-tv/build.sh # local test
+bash -c "CMAKE_BINARY_DIR=/opt/build/vt-tv \
+    VTK_DIR=/opt/build/vtk \
+    VT_TV_TESTS_ENABLED=ON \
+    VT_TV_COVERAGE_ENABLED=ON \
+    /home/thomas/repositories/vt-tv/build.sh"
 
 echo "VT-TV build success"
