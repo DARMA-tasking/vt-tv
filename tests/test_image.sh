80d18d9ac0f0362aa12cf0b3244756d67c477530
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-24 19:36:03 +0200

    #32: update test output and change image diff test tolerance to 2.0

--- tests/test_image.sh
+++ tests/test_image.sh
@@ -6,10 +6,10 @@ CURRENT_DIR="$(dirname -- "$(realpath -- "$0")")" # Current directory
 
 ACTUAL=${ACTUAL:-/home/thomas/repositories/vt-tv/output/tests/ccm_example0.png}
 EXPECTED=${EXPECTED:-$CURRENT_DIR/expected/ccm_example0.png}
-TOLERANCE=${TOLERANCE:-1.0}
+TOLERANCE=${TOLERANCE:-2.0}
 
 if [ ! -f "$ACTUAL" ]; then
-    >&2 echo "Image not found at "$ACTUAL
+    echo "Image not found at "$ACTUAL
     exit 1
 fi
 
@@ -17,7 +17,7 @@ pip install imgcompare --quiet 2>/dev/null
 
 DIFF=$(printf "%.2f" $(python -c 'import imgcompare; print(imgcompare.image_diff_percent("'$ACTUAL'", "'$EXPECTED'"));'))
 if { echo $TOLERANCE ; echo $DIFF ; } | sort -n -c 2>/dev/null; then
-    >&2 echo "Image diff = $DIFF%. Tolerance is $TOLERANCE"
+    echo "Image diff = $DIFF%. Tolerance is $TOLERANCE"
     exit 2
 fi
 
