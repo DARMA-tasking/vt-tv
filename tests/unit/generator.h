156b096666ccfa923fdccdc6aea33781d04896c8
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-07-26 16:05:08 +0200

    #32: fix using statements

--- tests/unit/generator.h
+++ tests/unit/generator.h
@@ -41,147 +41,148 @@
 //@HEADER
 */
 
-#include <yaml-cpp/yaml.h>
-
-#include <vt-tv/api/types.h>
-#include <vt-tv/api/rank.h>
-#include <vt-tv/api/object_work.h>
-#include <vt-tv/api/object_info.h>
 #include <vt-tv/api/info.h>
-
+#include <vt-tv/api/object_info.h>
+#include <vt-tv/api/object_work.h>
+#include <vt-tv/api/rank.h>
+#include <vt-tv/api/types.h>
 #include <vt-tv/utility/json_reader.h>
+#include <yaml-cpp/yaml.h>
 
 #include <string>
 
 namespace vt::tv::tests::unit {
+
+using Util = vt::tv::tests::unit::Util;
 
 /**
  * Testing Helper class that provide useful static methods to be used by the different
  * unit tests classes related to the api to generate some data in memory.
  */
 class Generator {
-    public:
+ public:
+  /**
+   * Make a map of new objects
+   */
+  static const std::unordered_map<ElementIDType, ObjectWork> makeObjects(
+      const int num_objects = rand() % 10, TimeType load = 2.0, int first_id = 0) {
+    auto object_work_map = std::unordered_map<ElementIDType, ObjectWork>();
+    for (auto object_id = first_id; object_id < first_id + num_objects; object_id++) {
+      // Make some ObjectWork instance
+      auto subphase_loads = std::unordered_map<SubphaseType, TimeType>();
+      auto user_defined = makeQOIVariants(5, "user_");
+      auto attributes = makeQOIVariants(5, "attr_");
+      auto object_work = ObjectWork(object_id, load, subphase_loads, user_defined, attributes);
+      object_work_map.insert(std::make_pair(object_id, object_work));
+    }
+    return object_work_map;
+  }
 
-        /**
-         * Make a map of new objects
-         */
-        static const std::unordered_map<ElementIDType, ObjectWork> makeObjects(const int num_objects = rand() % 10, TimeType load = 2.0, int first_id = 0) {
-            auto object_work_map = std::unordered_map<ElementIDType, ObjectWork>();
-            for (auto object_id = first_id; object_id < first_id + num_objects; object_id++) {
-                // Make some ObjectWork instance
-                auto subphase_loads = std::unordered_map<SubphaseType, TimeType>();
-                auto user_defined = makeQOIVariants(5, "user_");
-                auto attributes = makeQOIVariants(5, "attr_");
-                auto object_work = ObjectWork(object_id, load, subphase_loads, user_defined, attributes);
-                object_work_map.insert(std::make_pair(object_id, object_work));
-            }
-            return object_work_map;
-        }
+  /**
+   * Make a new phase
+   */
+  static const PhaseWork makePhase(PhaseType phase_id,
+                                   std::unordered_map<ElementIDType, ObjectWork> objects) {
+    PhaseWork phase = PhaseWork(phase_id, objects);
+    return phase;
+  }
 
-        /**
-         * Make a new phase
-         */
-        static const PhaseWork makePhase(PhaseType phase_id, std::unordered_map<ElementIDType, ObjectWork> objects) {
-            PhaseWork phase = PhaseWork(phase_id, objects);
-            return phase;
-        }
+  /**
+   * Make a map of new phases
+   */
+  static const std::unordered_map<PhaseType, PhaseWork> makePhases(
+      std::unordered_map<ElementIDType, ObjectWork> objects,
+      const uint64_t num_phases = rand() % 10) {
+    auto phase_info_map = std::unordered_map<PhaseType, PhaseWork>();
+    for (uint64_t phase_id = 0; phase_id < num_phases; phase_id++) {
+      phase_info_map.insert(std::make_pair(phase_id, makePhase(phase_id, objects)));
+    }
+    return phase_info_map;
+  }
 
-        /**
-         * Make a map of new phases
-         */
-        static const std::unordered_map<PhaseType, PhaseWork> makePhases(std::unordered_map<ElementIDType, ObjectWork> objects, const uint64_t num_phases = rand() % 10) {
-            auto phase_info_map = std::unordered_map<PhaseType, PhaseWork>();
-            for (uint64_t phase_id = 0; phase_id < num_phases; phase_id++) {
-                phase_info_map.insert(std::make_pair(phase_id, makePhase(phase_id, objects)));
-            }
-            return phase_info_map;
-        }
+  /**
+   * Make a map of new ranks
+   */
+  static const std::unordered_map<NodeType, Rank> makeRanks(
+      std::unordered_map<ElementIDType, ObjectWork> objects, int16_t num_ranks = rand() % 10,
+      uint64_t num_phases = rand() % 10) {
+    auto rank_map = std::unordered_map<NodeType, Rank>();
+    for (NodeType rank_id = 0; rank_id < num_ranks; rank_id++) {
+      auto rank = Rank(rank_id, makePhases(objects, num_phases));
+      rank_map.insert(std::make_pair(rank_id, rank));
+    }
+    return rank_map;
+  }
 
-        /**
-         * Make a map of new ranks
-         */
-        static const std::unordered_map<NodeType, Rank> makeRanks(std::unordered_map<ElementIDType, ObjectWork> objects, int16_t num_ranks = rand() % 10, uint64_t num_phases = rand() % 10) {
-            auto rank_map = std::unordered_map<NodeType, Rank>();
-            for (NodeType rank_id = 0; rank_id < num_ranks; rank_id++) {
-                auto rank = Rank(rank_id, makePhases(objects, num_phases));
-                rank_map.insert(std::make_pair(rank_id, rank));
-            }
-            return rank_map;
-        }
+  /**
+   * Make a map object info from an object map
+   */
+  static std::unordered_map<ElementIDType, ObjectInfo> makeObjectInfoMap(
+      const std::unordered_map<ElementIDType, ObjectWork> object_work_map, bool migratable = true) {
+    auto object_info_map = std::unordered_map<ElementIDType, ObjectInfo>();
+    std::vector<size_t> idx;
+    for (auto& it : object_work_map) {
+      ObjectInfo object_info = ObjectInfo(it.first, 0, migratable, idx);
+      object_info_map.insert(std::make_pair(it.first, object_info));
+    }
+    return object_info_map;
+  }
 
-        /**
-         * Make a map object info from an object map
-         */
-        static std::unordered_map<ElementIDType, ObjectInfo> makeObjectInfoMap (
-            const std::unordered_map<ElementIDType, ObjectWork> object_work_map,
-            bool migratable = true
-        ) {
-            auto object_info_map = std::unordered_map<ElementIDType, ObjectInfo>();
-            std::vector<size_t> idx;
-            for (auto& it: object_work_map) {
-                ObjectInfo object_info = ObjectInfo(it.first, 0, migratable, idx);
-                object_info_map.insert(std::make_pair(it.first, object_info));
-            }
-            return object_info_map;
-        }
+  static std::unordered_map<std::string, QOIVariantTypes> makeQOIVariants(
+      uint64_t num = rand() % 10, std::string key_prefix = "attr_",
+      std::string value_suffix = "_value") {
+    auto qoi_map = std::unordered_map<std::string, QOIVariantTypes>();
+    for (size_t i = 0; i < num; i++) {
+      qoi_map[key_prefix + std::to_string(i)] = key_prefix + std::to_string(i) + value_suffix;
+    }
+    return qoi_map;
+  }
 
-        static std::unordered_map<std::string, QOIVariantTypes> makeQOIVariants (
-            uint64_t num = rand() % 10,
-            std::string key_prefix = "attr_",
-            std::string value_suffix = "_value"
-        ) {
-            auto qoi_map = std::unordered_map<std::string, QOIVariantTypes>();
-            for (size_t i=0; i<num; i++) {
-                qoi_map[key_prefix + std::to_string(i)] = key_prefix + std::to_string(i) + value_suffix;
-            }
-            return qoi_map;
-        }
+  /**
+   * Make an Info instance
+   */
+  static const Info makeInfo(int num_objects = rand() % 100, int num_ranks = rand() % 10,
+                             uint64_t num_phases = 10) {
+    auto objects = makeObjects(num_objects);
+    auto ranks = makeRanks(objects, num_ranks, num_phases);
+    return Info(makeObjectInfoMap(objects), ranks);
+  }
 
-        /**
-         * Make an Info instance
-         */
-        static const Info makeInfo(int num_objects = rand() % 100, int num_ranks = rand() % 10, uint64_t num_phases = 10) {
-            auto objects = makeObjects(num_objects);
-            auto ranks = makeRanks(objects, num_ranks, num_phases);
-            return Info(makeObjectInfoMap(objects), ranks);
-        }
+  static Info loadInfoFromConfig(YAML::Node config) {
+    using JSONReader = ::vt::tv::utility::JSONReader;
 
-        static Info loadInfoFromConfig(YAML::Node config) {
-            using JSONReader = ::vt::tv::utility::JSONReader;
+    std::string input_dir =
+        Util::resolveDir(SRC_DIR, config["input"]["directory"].as<std::string>(), true);
+    int64_t n_ranks = config["input"]["n_ranks"].as<int64_t>();
+    Info info = Info();
 
-            std::string input_dir = Util::resolveDir(SRC_DIR, config["input"]["directory"].as<std::string>(), true);
-            int64_t n_ranks = config["input"]["n_ranks"].as<int64_t>();
-            Info info = Info();
+#ifdef VT_TV_OPENMP_ENABLED
+#if VT_TV_OPENMP_ENABLED
+#ifdef VT_TV_N_THREADS
+    const int threads = VT_TV_N_THREADS;
+#else
+    const int threads = 2;
+#endif  // VT_TV_N_THREADS
+    omp_set_num_threads(threads);
+    fmt::print("vt-tv: Using {} threads\n", threads);
+#pragma omp parallel for
+#endif
+#endif  // VT_TV_OPENMP_ENABLED
 
-            #ifdef VT_TV_OPENMP_ENABLED
-            #if VT_TV_OPENMP_ENABLED
-                #ifdef VT_TV_N_THREADS
-                const int threads = VT_TV_N_THREADS;
-                #else
-                const int threads = 2;
-                #endif // VT_TV_N_THREADS
-                omp_set_num_threads(threads);
-                fmt::print("vt-tv: Using {} threads\n", threads);
-                # pragma omp parallel for
-            #endif
-            #endif // VT_TV_OPENMP_ENABLED
-
-            for (int64_t rank = 0; rank < n_ranks; rank++) {
-                fmt::print("Reading file for rank {}\n", rank);
-                JSONReader reader{static_cast<NodeType>(rank)};
-                reader.readFile(input_dir + "data." + std::to_string(rank) + ".json");
-                auto tmpInfo = reader.parse();
-                #ifdef VT_TV_OPENMP_ENABLED
-                #if VT_TV_OPENMP_ENABLED
-                    #pragma omp critical
-                #endif
-                #endif
-                {
-                info.addInfo(tmpInfo->getObjectInfo(), tmpInfo->getRank(rank));
-                }
-            }
-            return info;
-        }
+    for (int64_t rank = 0; rank < n_ranks; rank++) {
+      fmt::print("Reading file for rank {}\n", rank);
+      JSONReader reader{static_cast<NodeType>(rank)};
+      reader.readFile(input_dir + "data." + std::to_string(rank) + ".json");
+      auto tmpInfo = reader.parse();
+#ifdef VT_TV_OPENMP_ENABLED
+#if VT_TV_OPENMP_ENABLED
+#pragma omp critical
+#endif
+#endif
+      { info.addInfo(tmpInfo->getObjectInfo(), tmpInfo->getRank(rank)); }
+    }
+    return info;
+  }
 };
 
-}
+}  // namespace vt::tv::tests::unit
