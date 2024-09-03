3de447d6cd7bdf0b9108d6b14d430a02fa477129
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-03 17:28:39 +0200

    #97: refactor use vtk_build script also on mac os runner

--- ci/build_vtk.sh
+++ ci/build_vtk.sh
@@ -4,20 +4,36 @@
 
 set -ex
 
-. /etc/lsb-release
-VTK_OPENGL_HAS_OSMESA=ON
+VTK_OPENGL_HAS_OSMESA=OFF
 VTK_USE_X=OFF
+VTK_USE_COCOA=OFF
 VTK_SRC_DIR=${VTK_SRC_DIR:-"/opt/src/vtk"}
 VTK_DIR=${VTK_DIR:-"/opt/build/vtk"}
 
 mkdir -p $VTK_DIR
 pushd $VTK_DIR
 
-# Issue with Ubuntu 24.04 / OSMESA : glew build error. Use X instead.
-if [ "$DISTRIB_ID" == "Ubuntu" -a "$DISTRIB_RELEASE" == "24.04" ]; then
-  echo "Disabling VTK_OPENGL_HAS_OSMESA (not supported in Ubuntu 24.04). Using DVTK_USE_X instead."
-  VTK_OPENGL_HAS_OSMESA=OFF
-  VTK_USE_X=ON
+unameOut=$(uname -a)
+case "${unameOut}" in
+    Linux*)    OS="Linux";;
+    Darwin*)   OS="MacOSX";;
+    *)         OS="UNKNOWN:${unameOut}"
+esac
+
+if [ "$OS" == "Linux" ]; then
+  . /etc/lsb-release
+  # Issue with Ubuntu 24.04 / OSMESA : glew build error. Use X instead.
+  if [ "$DISTRIB_ID" == "Ubuntu" -a "$DISTRIB_RELEASE" == "24.04" ]; then
+    echo "Disabling VTK_OPENGL_HAS_OSMESA (not supported in Ubuntu 24.04). Using DVTK_USE_X instead."
+    VTK_USE_X=ON
+  else
+    VTK_OPENGL_HAS_OSMESA=ON
+  end
+  # MacOs
+elif [ "$OS" == "MacOSX" ]; then
+  VTK_USE_COCOA=ON
+else
+  VTK_OPENGL_HAS_OSMESA=ON
 fi
 
 cmake \
@@ -27,7 +43,7 @@ cmake \
   -DVTK_DEFAULT_RENDER_WINDOW_OFFSCREEN:BOOL=ON \
   -DVTK_USE_X:BOOL=$VTK_USE_X \
   -DVTK_USE_WIN32_OPENGL:BOOL=OFF \
-	-DVTK_USE_COCOA:BOOL=OFF \
+	-DVTK_USE_COCOA:BOOL=$VTK_USE_COCOA \
 	-DVTK_USE_SDL2:BOOL=OFF \
   -DVTK_Group_Rendering:BOOL=OFF \
   -DBUILD_SHARED_LIBS:BOOL=ON \
