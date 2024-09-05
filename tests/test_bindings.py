23ef1e611e5ab5822c2cd161ad2221849593e10f
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-05 17:24:12 +0200

    #97: format bindings test python script

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -1,35 +1,38 @@
+"""This module calls vttv module to test that vttv bindings work as expected"""
 import json
+import os
+
 import yaml
 import vttv
 
-import sys
-import os
 
 # source dir is the directory a level above this file
 source_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(f'{source_dir}/tests/test_bindings_conf.yaml', 'r') as stream:
-  try:
-    params = yaml.safe_load(stream)
-  except yaml.YAMLError as exc:
-    print(exc)
+with open(f'{source_dir}/tests/test_bindings_conf.yaml', 'r', encoding='utf-8') as stream:
+    try:
+        params = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+        exit(1)
 
 # make output_visualization_dir directory parameter absolute
-if (not os.path.isabs(params["visualization"]["output_visualization_dir"])):
-  params["visualization"]["output_visualization_dir"] = source_dir + "/" + params["visualization"]["output_visualization_dir"]
-  print("Python bindings tests output directory: " + params["visualization"]["output_visualization_dir"])
+if not os.path.isabs(params["visualization"]["output_visualization_dir"]):
+    params["visualization"]["output_visualization_dir"] = source_dir + \
+        "/" + params["visualization"]["output_visualization_dir"]
 
 params_serialized = yaml.dump(params["visualization"])
 
-n_ranks = params["visualization"]["x_ranks"] * params["visualization"]["y_ranks"] * params["visualization"]["z_ranks"]
+n_ranks = params["visualization"]["x_ranks"] * \
+    params["visualization"]["y_ranks"] * params["visualization"]["z_ranks"]
 rank_data = []
 
 for rank in range(n_ranks):
-  with open(f'{source_dir}/data/lb_test_data/data.{rank}.json', 'r') as f:
-    data = json.load(f)
+    with open(f'{source_dir}/data/lb_test_data/data.{rank}.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
 
-  data_serialized = json.dumps(data)
+    data_serialized = json.dumps(data)
 
-  rank_data.append((data_serialized))
+    rank_data.append((data_serialized))
 
 vttv.tvFromJson(rank_data, params_serialized, n_ranks)
