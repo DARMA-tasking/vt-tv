e8407e452792ff0d6e341591290e94b3ad946496
Thomas Dutheillet-Lamonthézie <thomas.dutheillet@ng-analytics.com>
2024-09-06 10:38:08 +0200

    #97: clean shell scripts for CI

--- ci/xvfb_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This script activates X virtual frame buffer display
-
-XVFB_DISPLAY="${1:-:99}"
-echo "Starting X Virtual Frame Buffer ($XVFB_DISPLAY)..."
-Xvfb $XVFB_DISPLAY -screen 0 1024x768x24 -nolisten tcp > /dev/null 2>&1 &
-sleep 1
