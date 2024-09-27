6ae5691211b07d7ab9234d101bbac7635231e034
Maxime N <maxime.noyelle@ng-analytics.com>
2024-09-27 15:43:31 +0200

    Merge commit '6617f3a3aebf41bcd790182ff8ae0e73f4d600f4' into 7-add-json-validation

--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -1,32 +1,43 @@
 """This module calls vttv module to test that vttv bindings work as expected"""
-import json
 import os
-import sys
 import subprocess
+import json
+import sys
 import yaml
 import vttv
 
 # source dir is the directory a level above this file
 source_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-with open(f'{source_dir}/tests/test_bindings_conf.yaml', 'r', encoding='utf-8') as stream:
+# Read the YAML config file
+with open(f"{source_dir}/tests/test_bindings_conf.yaml", 'r', encoding="utf-8") as stream:
     try:
         params = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
-        exit(1)
+
+# Check main key is "visualization"
+if "visualization" not in params:
+    print(
+        "The YAML configuration file is not valid: "+\
+        "missing required paramater \"visualization\""
+    )
+    sys.exit(1)
 
 # make output_visualization_dir directory parameter absolute
-if not os.path.isabs(params["visualization"]["output_visualization_dir"]):
-    params["visualization"]["output_visualization_dir"] = source_dir + \
-        "/" + params["visualization"]["output_visualization_dir"]
+if "output_visualization_dir" in params["visualization"]:
+    if not os.path.isabs(params["visualization"]["output_visualization_dir"]):
+        params["visualization"]["output_visualization_dir"] = source_dir + \
+            "/" + params["visualization"]["output_visualization_dir"]
 
+# Serialize visualization parameters
 params_serialized = yaml.dump(params["visualization"])
 
+# Calculate n_ranks
 n_ranks = params["visualization"]["x_ranks"] * \
     params["visualization"]["y_ranks"] * params["visualization"]["z_ranks"]
-rank_data = []
 
+rank_data = []
 for rank in range(n_ranks):
     # JSON data file for rank
     datafile = f'{source_dir}/data/lb_test_data/data.{rank}.json'
@@ -50,11 +61,12 @@ for rank in range(n_ranks):
         sys.exit(1)
 
     # Read JSON data file
-    with open(datafile, 'r', encoding='utf-8') as f:
+    with open(datafile, 'r', encoding="utf-8") as f:
         data = json.load(f)
     data_serialized = json.dumps(data)
 
-    # Add the rank data line
-    rank_data.append((data_serialized))
+    # Add serialized data into the rank
+    rank_data.append((json.dumps(data)))
 
+# Launch VT TV from JSON data
 vttv.tvFromJson(rank_data, params_serialized, n_ranks)
