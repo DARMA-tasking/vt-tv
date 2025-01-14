5c33af754e41a5c079c2433d1a3ffef9e73b3ca7
Pierre Pebay <pebay.pierre@gmail.com>
2025-01-14 17:24:59 +0100

    #133: ci: Install necessary pip packages for JSON validation on MacOS

--- ci/python_build.sh
+++ ci/python_build.sh
@@ -11,6 +11,16 @@ VTK_DIR="${VTK_DIR:-$PARENT_DIR/vtk/build}"
 
 VT_TV_SRC_DIR=${VT_TV_SRC_DIR:-$PARENT_DIR}
 
+$CONDA_PATH/bin/conda init bash
+$CONDA_PATH/bin/conda init zsh
+if [ -f ~/.zshrc ]; then . ~/.zshrc; fi
+if [ -f ~/.profile ]; then . ~/.profile; fi
+if [ -f ~/.bashrc ]; then . ~/.bashrc; fi
+
+echo "Conda path: $(which conda)"
+echo "Conda version: $(conda --version)"
+conda deactivate
+
 for env in $(conda env list | grep -E '^py' | perl -lane 'print $F[-1]' | xargs ls -ld | perl -lane 'print $F[-1]' | sed -E 's|^.*/(.*)$|\1|'); do
     echo "::group::Build Python Bindings (${env})"
 
