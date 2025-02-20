6c6c46f9b1e1a2afc6224e42628601e287a7b2b1
Pierre Pebay <pebay.pierre@gmail.com>
2025-02-20 22:12:22 +0100

    #125: Fix install target and rename standalone app

--- cmake/load_vtk_package.cmake
+++ cmake/load_vtk_package.cmake
@@ -20,3 +20,22 @@ find_package(
 message(STATUS "VTK version: ${VTK_VERSION}")
 
 message(STATUS "VTK libraries: ${VTK_LIBRARIES}")
+
+foreach(VTK_LIBRARY ${VTK_LIBRARIES})
+  get_target_property(VTK_RUNTIME_LIBRARY_DIR ${VTK_LIBRARY} LOCATION)
+
+  list(
+    APPEND
+    VTK_RUNTIME_LIBRARY_DIRS
+    "${VTK_RUNTIME_LIBRARY_DIR}"
+  )
+endforeach()
+
+if(NOT VTK_RUNTIME_LIBRARY_DIRS)
+  message(FATAL_ERROR "VTK runtime library paths could not be determined!")
+endif()
+
+install(
+    FILES ${VTK_RUNTIME_LIBRARY_DIRS}
+    DESTINATION lib
+)
