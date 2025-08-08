cca1bad460ffc3cbad5f9c15aaf969ea0ca61f5f
Jacob Domagala <domagala.jacob@gmail.com>
2025-08-08 11:56:12 +0200

    #136: Adjust the ci build and test scripts to do actually what they suggest - build_cpp.sh will only build cpp project and test_cpp will only run the gtest (without again building it)

---
+++ ci/build_cpp.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# This script builds vt-tv for CI including tests and coverage
+
+set -ex
+
+source_dir=${1}
+build_dir=${2}
+
+if hash ccache &>/dev/null
+then
+    use_ccache=true
+fi
+
+if test "$use_ccache"
+then
+    { echo -e "===\n=== ccache statistics before build\n==="; } 2>/dev/null
+    ccache -s
+else
+    { echo -e "===\n=== ccache not found, compiling without it\n==="; } 2>/dev/null
+fi
+
+mkdir -p "${build_dir}"
+pushd "${build_dir}"
+
+export VT_TV=${source_dir}
+export VT_TV_BUILD=${build_dir}/vt-tv
+
+mkdir -p "$VT_TV_BUILD"
+cd "$VT_TV_BUILD"
+rm -Rf ./*
+
+cmake -B "${VT_TV_BUILD}" \
+   -DCMAKE_BUILD_TYPE:STRING="${VT_TV_BUILD_TYPE:-Release}" \
+   -DVTK_DIR="${VTK_DIR:-/opt/vtk}" \
+   -DCMAKE_C_COMPILER="${CC:-gcc}" \
+   -DCMAKE_CXX_COMPILER="${CXX:-g++}" \
+   -DCMAKE_INSTALL_PREFIX="${VT_TV_INSTALL_DIR:-}" \
+   -DVT_TV_WERROR_ENABLED="${VT_TV_WERROR_ENABLED:-ON}" \
+   -DVT_TV_TESTS_ENABLED="${VT_TV_TESTS_ENABLED:-ON}" \
+   -DVT_TV_COVERAGE_ENABLED="${VT_TV_COVERAGE_ENABLED:-OFF}" \
+   -DVT_TV_PYTHON_BINDINGS_ENABLED="${VT_TV_PYTHON_BINDINGS_ENABLED:-OFF}" \
+   -DPython_EXECUTABLE="$(which python)" \
+   -DPython_INCLUDE_DIRS="$(python -c "import sysconfig; print(sysconfig.get_path('include'))")" \
+   "${VT_TV}"
+
+time cmake --build . --target install
+
+if test "$use_ccache"
+then
+    { echo -e "===\n=== ccache statistics after build\n==="; } 2>/dev/null
+    ccache -s
+fi
---
+++ ci/test_cpp.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -exo pipefail
+
+source_dir=${1}
+build_dir=${2}
+
+export VT_TV=${source_dir}
+export VT_TV_BUILD=${build_dir}/vt-tv
+
+export VT_TV_OUTPUT_DIR="${VT_TV_OUTPUT_DIR:-$source_dir/output}"
+
+pushd "$VT_TV_BUILD"
+
+# Active conda env that contains needed python packages
+eval "$("$CONDA_PATH/bin/conda" shell.bash hook)"
+conda activate py3.12
+
+#Run tests
+mkdir -p "$VT_TV_OUTPUT_DIR"
+# Tests
+echo "> Running tests..."
+# Run GTest unit tests and display detail for failing tests
+GTEST_OPTIONS=""
+
+if [ "$VT_TV_TEST_REPORT" != "" ]; then
+  echo "Generating JUnit report..."
+  GTEST_OPTIONS="$GTEST_OPTIONS --gtest_output=\"xml:$VT_TV_OUTPUT_DIR/$VT_TV_TEST_REPORT\""
+fi
+
+if [ "$VT_TV_RUN_TESTS_FILTER" != "" ]; then
+  echo "Filtering Tests ($VT_TV_RUN_TESTS_FILTER)..."
+  GTEST_OPTIONS="$GTEST_OPTIONS --gtest_filter=\"$VT_TV_RUN_TESTS_FILTER\""
+fi
+
+gtest_cmd="\"$VT_TV_BUILD_DIR/tests/unit/AllTests\" $GTEST_OPTIONS"
+echo "Run GTest..."
+eval "$gtest_cmd" || true
+echo "Tests done."
+
+if [ "$VT_TV_COVERAGE_ENABLED" == "ON" ]; then
+  lcov --gcov-tool /usr/bin/gcov-12 --directory "$VT_TV_BUILD_DIR" --capture --output-file coverage.info
+  lcov --remove coverage.info '/usr/*' '/opt/vtk/*' --output-file coverage.info
+  lcov --list coverage.info
+fi
+
+
+# Add artifacts
+VT_TV_ARTIFACTS_DIR="/tmp/artifacts"
+mkdir -p $VT_TV_ARTIFACTS_DIR
+
+# > go to output directory
+pushd "$VT_TV_OUTPUT_DIR"
+
+# > add the unit tests report artifact
+cp "junit-report.xml" $VT_TV_ARTIFACTS_DIR/ || true
+
+# > add mesh files and png artifacts
+if [ -d "$VT_TV_TESTS_OUTPUT_DIR" ]; then
+    cp "$VT_TV_TESTS_OUTPUT_DIR/"*".vtp"  $VT_TV_ARTIFACTS_DIR/
+    cp "$VT_TV_TESTS_OUTPUT_DIR/"*".png"  $VT_TV_ARTIFACTS_DIR/
+fi
+
+popd
+
+# list artifacts dir content
+ls $VT_TV_ARTIFACTS_DIR
