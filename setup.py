1e56b94f42d8658f2a5362f4d8ab6da3f2c95e5e
Pierre Pebay <pebay.pierre@gmail.com>
2026-01-19 18:40:21 +0100

    #178: Specify nanobind requirement in python build

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ class CMakeBuild(build_ext):
       raise RuntimeError("Environment variable VT_TV_N_THREADS must be an integer")
 
     cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                  '-DPYTHON_EXECUTABLE=' + sys.executable,
+                  '-DPython_EXECUTABLE=' + sys.executable,
                   '-DVT_TV_PYTHON_BINDINGS_ENABLED=ON',
                   '-DVTK_DIR=' + vtk_dir,
                   '-DVT_TV_N_THREADS=' + str(n_threads)]
@@ -78,4 +78,5 @@ setup(
   package_dir={'': build_dir},
   packages=find_packages(build_dir),
   zip_safe=False,
+  # install_requires=["nanobind>=2.0"],
 )
